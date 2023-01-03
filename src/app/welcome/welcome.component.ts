cd44fd45d94bf76ff913e1e7d1be2d74af63f0b1
kajalsan23 <48622470+kajalsan23@users.noreply.github.com>
2023-01-03 14:36:41 +0530

    2 files modified

--- src/app/welcome/welcome.component.ts
+++ src/app/welcome/welcome.component.ts
@@ -1,29 +1,32 @@
-import { Component, OnInit,ViewChild,ElementRef } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-welcome',
   templateUrl: './welcome.component.html',
-  styleUrls: ['./welcome.component.scss']
+  styleUrls: ['./welcome.component.scss'],
 })
 export class WelcomeComponent implements OnInit {
-
-
   rules = [
-   "Correct question gives you 10 Points",
-    "InCorrect question gives you -10 Points",
-    "You will have 10 sec to answer each questions",
-    "Refreshing the page will reset"
-  ]
+    'Correct question gives you 10 Points',
+    'InCorrect question gives you -10 Points',
+    'You will have 10 sec to answer each questions',
+    'Refreshing the page will reset',
+  ];
 
-  @ViewChild('name') nameKey!:ElementRef
+  @ViewChild('name') nameKey!: ElementRef;
 
-  constructor() { }
+  constructor(private router: Router) {}
 
-  ngOnInit(): void {
+  ngOnInit(): void {}
+
+  startQuiz() {
+    localStorage.setItem('name', this.nameKey.nativeElement.value);
+    console.log(this.nameKey.nativeElement.value);
+    if (this.nameKey.nativeElement.value != '') {
+      this.router.navigate(['/question']);
+    } else {
+      alert('Please Enter your Name');
+    }
   }
-
-  startQuiz(){
-    localStorage.setItem("name",this.nameKey.nativeElement.value)
-  }
-
 }
